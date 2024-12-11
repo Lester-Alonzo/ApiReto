ee58fb5a46c0e9d1a43f02d4bd96c6ed200afd4b
Lester-Alonzo <lestergeo96@gmail.com>
2024-12-11 11:40:38 -0600

    [ADD] Se agrego sequelize, sqlServer y keydb que es igual a redis

---
+++ src/db/mssql.ts
@@ -0,0 +1,28 @@
+import {Sequelize, Model, DataTypes} from '@sequelize/core'
+import {MsSqlDialect} from '@sequelize/mssql'
+
+const sequelize = new Sequelize({
+    dialect:MsSqlDialect,
+    server:"localhost",
+    port:1433,
+    database: "EcomerceV3",
+    authentication: {
+        type:"default",
+        options: {
+            userName: "sa",
+            password: "L@grasaesgrasa2025",
+        }
+    },
+    encrypt:true,
+    trustServerCertificate:true
+})
+
+async function SQLServerPing() {
+    try {
+    const respons = await sequelize.authenticate()
+    console.log(respons)
+    } catch (error) {
+        console.error(error)
+    }
+}
+(async () => await SQLServerPing())()
