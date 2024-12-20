d2cf12b5c6b8d266e0dbb1717999b1468d518d51
Lester-Alonzo <lestergeo96@gmail.com>
2024-12-20 13:08:16 -0600

    cmabios README

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { app } from "./config"
 app.listen(5898, () =>
   console.log(`
     http://localhost:5898,
-    Routes:
-    /api-docs : Documentacion en swagger con OpenApi 2.0
+    /api-docs : Documentacion en swagger con OpenApi 3.0
     `),
 )
